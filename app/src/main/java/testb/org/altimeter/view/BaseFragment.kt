7be8e6a6d05cdc8d3c5990d452b078adaf7fe8ba
Gensen Choy <gensen.choy@gmail.com>
2017-06-28 15:59:00 -0700

    Fixed BaseFragment to be open class

--- app/src/main/java/testb/org/altimeter/view/BaseFragment.kt
+++ app/src/main/java/testb/org/altimeter/view/BaseFragment.kt
@@ -2,7 +2,7 @@ package testb.org.altimeter.view
 
 import android.support.v4.app.Fragment
 
-class BaseFragment : Fragment() {
+open class BaseFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         val refWatcher = BaseApplication.getRefWatcher(activity)
