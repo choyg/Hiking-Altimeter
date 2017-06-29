0f7acc1d41f0012aac0f00bdb77829ba7fe8afde
Gensen Choy <gensen.choy@gmail.com>
2017-06-28 20:57:29 -0700

    Fragments automatically call to attach/detach presenter Fragments automatically clear disposables in onDestory

--- app/src/main/java/testb/org/altimeter/view/BaseFragment.kt
+++ app/src/main/java/testb/org/altimeter/view/BaseFragment.kt
@@ -1,11 +1,25 @@
 package testb.org.altimeter.view
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.View
+import io.reactivex.disposables.CompositeDisposable
 
-open class BaseFragment : Fragment() {
+abstract class BaseFragment : Fragment() {
+    protected val compositeDeposible = CompositeDisposable()
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        getPresenter().attachView()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
+        getPresenter().detachView()
+        compositeDeposible.clear()
+
         val refWatcher = BaseApplication.getRefWatcher(activity)
         refWatcher.watch(this)
     }
+
+    protected abstract fun getPresenter(): BasePresenter
 }
