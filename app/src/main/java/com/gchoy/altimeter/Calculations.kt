33ceb7385200db94adc9829890e1fbe65742702d
Gensen Choy <gensen.choy@gmail.com>
2017-06-29 15:11:28 -0700

    Changed calculations to be package level functions

--- app/src/main/java/com/gchoy/altimeter/Calculations.kt
+++ app/src/main/java/com/gchoy/altimeter/Calculations.kt
@@ -1,15 +1,12 @@
 package com.gchoy.altimeter
 
-class Calculations {
+val SEA_TEMPERATURE = 288.15
+val DEFAULT_SEA_PRESSURE = 1013.25
 
-    val SEA_TEMPERATURE = 288.15
-    val DEFAULT_SEA_PRESSURE = 1013.25
+fun calculateAltitude(currentPressure: Double, seaLevelPressure: Double = DEFAULT_SEA_PRESSURE): Double {
+    return ((SEA_TEMPERATURE - (SEA_TEMPERATURE / (Math.pow(seaLevelPressure / currentPressure, 0.19026324)))) / 0.0065)
+}
 
-    fun calculateAltitude(seaLevelPressure: Double = DEFAULT_SEA_PRESSURE, currentPressure: Double): Double {
-        return ((SEA_TEMPERATURE - (SEA_TEMPERATURE / (Math.pow(seaLevelPressure / currentPressure, 0.19026324)))) / 0.0065)
-    }
-
-    fun calculateQNH(knownAltitude: Double, currentPressure: Double): Double {
-        return currentPressure * Math.pow(SEA_TEMPERATURE / (SEA_TEMPERATURE - 0.0065 * knownAltitude), 5.25587611)
-    }
+fun calculateQNH(knownAltitude: Double, currentPressure: Double): Double {
+    return currentPressure * Math.pow(SEA_TEMPERATURE / (SEA_TEMPERATURE - 0.0065 * knownAltitude), 5.25587611)
 }
